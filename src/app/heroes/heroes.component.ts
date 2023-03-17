4b590ee207e62b8c0a357856bbd94d9e17f4ebd9
Ching-Kai <65207577+Ching-Kai@users.noreply.github.com>
2023-03-17 11:58:38 +0800

    4 files modified, 5 added,step4AddServices

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes=HEROES;
+export class HeroesComponent implements OnInit {
 
-  hero : Hero ={
-      id:1,
-      name:'Windstorm'
-  }
-
-
-  constructor() { }
-  //ngOnInit() 是一個生命週期鉤子，Angular 在建立完元件後很快就會呼叫 ngOnInit()。這裡是放置初始化邏輯的好地方。
-  ngOnInit(): void {
-  }
   selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
